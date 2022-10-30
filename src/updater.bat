220926658590f81be89b8e2ee7a367fafaa28e56
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2022-10-30 16:06:37 +0100

    Correction d'un bug

--- src/updater.bat
+++ src/updater.bat
@@ -2,6 +2,6 @@
 title Mise à jour de Yuzu Cheats Manager
 echo Patientez...
 del /f /q YuzuCheatsManager.exe
-curl "https://github.com/Luckyluka17/YuzuCheatsManager/releases/latest/download/YuzuCheatsManager.exe" -o "%cd%\YuzuCheatsManager.exe"
+powershell Invoke-WebRequest https://github.com/Luckyluka17/YuzuCheatsManager/releases/latest/download/YuzuCheatsManager.exe -o YuzuCheatsManager.exe
 start YuzuCheatsManager.exe
 exit
