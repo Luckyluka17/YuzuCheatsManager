a1d09d669b4770353d0782efa75dc98edea2230c
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2022-12-28 18:51:42 +0100

    Modifications de l'updater

--- src/updater.bat
+++ src/updater.bat
@@ -1,6 +1,7 @@
 @echo off
-title Mise à jour de Yuzu Cheats Manager
-echo Patientez...
+title Gestion des mises à jours
+echo Patientez, YuzuCheatsManager effectue une mise à jour...
+echo Ne fermez pas cette fenêtre !
 del /f /q YuzuCheatsManager.exe
 powershell Invoke-WebRequest https://github.com/Luckyluka17/YuzuCheatsManager/releases/latest/download/YuzuCheatsManager.exe -o YuzuCheatsManager.exe
 start YuzuCheatsManager.exe
